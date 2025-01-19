57e785d39d831bd42356a360125d5a463e836fb9
Nauval Gunawan <nauvalgunawan5@gmail.com>
2025-01-19 21:47:04 +0700

    Fitur Login & Middleware | Vue JS + Laravel [3/18]

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Schema::disableForeignKeyConstraints();
+        // User::truncate();
+        // Schema::enableForeignKeyConstraints();
+
+        // User::insert([
+        //     'name' => 'manager',
+        //     'email' => 'manager@email.com',
+        //     'password' => bcrypt('password'),
+        //     'role_id' => 4,
+        //     'created_at' => now(),
+        //     'updated_at' => now()
+        // ]);
+    }
+}
